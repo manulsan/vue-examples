90c80aae22e567b8ae3a037071447c629cf3b5b2
Alex <31757599+manulsan@users.noreply.github.com>
2022-03-24 11:26:04 +0900

    Create example.vue

---
+++ example.vue
@@ -0,0 +1,14 @@
+<script>
+export	default {
+  data() {
+    return {
+      message :'Hello World'
+    }
+  }
+}
+</script>
+
+<template>
+<h1>   {{message}}  </h1>
+  <h1>   {{message.split('').reverse().join('')}}  </h1>
+</template>
