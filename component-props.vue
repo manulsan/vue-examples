9f773593227f8bab03c7525d1837721c0e4f0aa8
Alex <31757599+manulsan@users.noreply.github.com>
2022-03-24 12:50:12 +0900

    Create component-props.vue

---
+++ component-props.vue
@@ -0,0 +1,17 @@
+<script>
+import ChildComp from './ChildComp.vue'
+export default {
+  components: {
+    ChildComp
+  },
+  data() {
+    return {
+      greeting: 'Hello from parent'
+    }
+  }
+}
+</script>
+
+<template>
+  <ChildComp :msg="greeting" />
+</template>
