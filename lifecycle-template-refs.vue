3a3d5d6f98988f76c7c3a2a407096b419c45d602
Alex <31757599+manulsan@users.noreply.github.com>
2022-03-24 12:24:07 +0900

    Create lifecycle-template-refs.vue

---
+++ lifecycle-template-refs.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  mounted() {
+    this.$refs.p1.textContent = 'mounted!'
+    this.$refs.p2.textContent = 'mounted! 2'
+  }
+}
+</script>
+
+<template>
+  <p ref="p1">hello</p>
+  <p ref="p2">hello p2</p>
+</template>
