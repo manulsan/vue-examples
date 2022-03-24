faf930ca9abd32a611ee922638cc94422c79cbeb
Alex <31757599+manulsan@users.noreply.github.com>
2022-03-24 11:38:45 +0900

    Create attributeBindings.vue

---
+++ attributeBindings.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      titleClass:'title'
+    }
+  }
+}
+</script>
+
+<template>  
+  <h1 v-bind:class="titleClass">Make me red</h1>        
+</template>
+
+<style>
+  .title {
+    color: red;
+  }
+</style>
